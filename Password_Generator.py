563a32169a29e77cff6b1b814e0b0403e20deec7
sergiomuse <sergio.ortiz.neri@bbva.com>
2021-07-27 10:44:57 -0500

    Sintaxis

--- Password_Generator.py
+++ Password_Generator.py
@@ -6,8 +6,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?")) 
-nr_symbols = int(input(f"How many symbols would you like?"))
-nr_numbers = int(input(f"How many numbers would you like?"))
+nr_symbols = int(input("How many symbols would you like?"))
+nr_numbers = int(input("How many numbers would you like?"))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
